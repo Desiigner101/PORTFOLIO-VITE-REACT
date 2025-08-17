8a6bba57683bdbe805847f0ce84d503e54b0a7d2
KaizenG <desiigner4074@gmail.com>
2025-08-17 15:59:27 +0800

    added theme toggle and background (partial)

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+//id, size, x, y, opacity, animationDuration
+
+
+
+export const StartBackground = () => {
+    const [stars, setStarts] = useState()
+
+    {/* function that generate stars in the background */}
+    const generateStars = () => {
+        const numberOfStars = Math.floor(window.innerWidth * window.innerHeight / 10000)
+    }
+
+    return <div></div>
+}
--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  /*
   resolve:{
     alias:{
       "@": path.resolve(_dirname, "./src"),
     }
   }
+  */
 })
